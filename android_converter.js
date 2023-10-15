c1b2581fbcac9d733b19767f07e4aae40fcff25f
Andrew <39156362+Andrew2120@users.noreply.github.com>
2023-10-15 16:00:20 +0300

    Update android_converter.js

--- android_converter.js
+++ android_converter.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-let json = require('./tokens.json');
+let json = require('./output-tokens.json');
 
 let objectTypeCashe = {};
 let importLibs = "import androidx.compose.ui.unit.*\n"+
