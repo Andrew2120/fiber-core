b6eafb0c8ca0a80b36ffdeb541c794c376ddeeb5
Andrew <39156362+Andrew2120@users.noreply.github.com>
2023-10-16 21:35:49 +0300

    Update android_converter.js

--- android_converter.js
+++ android_converter.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-let json = require('./test.json');
+let json = require('./output-tokens.json');
 
 let importLibs = "import androidx.compose.ui.unit.*\n"+
                  "import androidx.compose.ui.graphics.Color\n"+
