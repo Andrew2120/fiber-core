7fd0589503cfc83cfade1183181d65cafe99c751
Andrew2120 <andrew-2120@live.com>
2023-10-16 20:03:44 +0000

    update tokens

--- BTokens.kt
+++ BTokens.kt
@@ -1,10 +1,473 @@
 import androidx.compose.ui.unit.*
 import androidx.compose.ui.graphics.Color
 import androidx.core.graphics.toColorInt
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalDensity
 
-object BTokens {
+object Content {
 
+val _primary = Color("#272e35".toColorInt())
+val _secondary = Color("#555f6d".toColorInt())
+val _tertiary = Color("#7e8c9a".toColorInt())
+val _disabled = Color("#9ea8b3".toColorInt())
+val _primary_inverse = Color("#ffffff".toColorInt())
+val _secondary_inverse = Color("#ffffffa3".toColorInt())
+val _tertiary_inverse = Color("#ffffff66".toColorInt())
+val _disabled_inverse = Color("#ffffff3d".toColorInt())
+val _info_primary = Color("#113997".toColorInt())
+val _info_secondary = Color("#3061d5".toColorInt())
+val _success_primary = Color("#b3b7b0".toColorInt())
+val _success_secondary = Color("#347434".toColorInt())
+val _warning_primary = Color("#3f2828".toColorInt())
+val _warning_secondary = Color("#f59638".toColorInt())
+val _danger_primary = Color("#30723a".toColorInt())
+val _danger_secondary = Color("#279e10".toColorInt())
+val _brand_primary = Color("#113997".toColorInt())
+val _brand_secondary = Color("#3061d5".toColorInt())
+}
+
+
+
+object Border {
+
+val _default = Color("#eaedf0".toColorInt())
+val _defaultA = Color("#10284717".toColorInt())
+val _inverse = Color("#ffffff".toColorInt())
+val _neutral_strong = Color("#555f6d".toColorInt())
+val _neutral_subtle = Color("#cfd6dd".toColorInt())
+val _info_strong = Color("#3061d5".toColorInt())
+val _info_subtle = Color("#ccdcff".toColorInt())
+val _success_strong = Color("#347434".toColorInt())
+val _success_subtle = Color("#c6ecc6".toColorInt())
+val _warning_strong = Color("#f59638".toColorInt())
+val _warning_subtle = Color("#ffd4a8".toColorInt())
+val _danger_strong = Color("#279e10".toColorInt())
+val _danger_subtle = Color("#f72626".toColorInt())
+}
+
+
+
+object Background {
+
+val _default = Color("#ffffff".toColorInt())
+val _inverse = Color("#272e35".toColorInt())
+val _neutral_strong = Color("#555f6d".toColorInt())
+val _neutral_muted = Color("#dee3e7".toColorInt())
+val _neutral_onSubtle = Color("#eaedf0".toColorInt())
+val _neutral_subtle = Color("#f5f7f9".toColorInt())
+val _neutral_surface = Color("#fcfcfd".toColorInt())
+val _info_strong = Color("#3061d5".toColorInt())
+val _info_muted = Color("#d6e3ff".toColorInt())
+val _info_onSubtle = Color("#e5eeff".toColorInt())
+val _info_subtle = Color("#f5f8ff".toColorInt())
+val _info_surface = Color("#fafbff".toColorInt())
+val _success_strong = Color("#347434".toColorInt())
+val _success_muted = Color("#cff2cf".toColorInt())
+val _success_onSubtle = Color("#dff6df".toColorInt())
+val _success_subtle = Color("#f4fbf4".toColorInt())
+val _success_surface = Color("#fbfefb".toColorInt())
+val _warning_strong = Color("#f59638".toColorInt())
+val _warning_muted = Color("#fcdec0".toColorInt())
+val _warning_onSubtle = Color("#ffe8d1".toColorInt())
+val _warning_subtle = Color("#fff5eb".toColorInt())
+val _warning_surface = Color("#fffcfa".toColorInt())
+val _danger_strong = Color("#279e10".toColorInt())
+val _danger_muted = Color("#d15a5a".toColorInt())
+val _danger_onSubtle = Color("#fee7e7".toColorInt())
+val _danger_subtle = Color("#fef5f5".toColorInt())
+val _danger_surface = Color("#fffafa".toColorInt())
+val _brand_strong = Color("#3061d5".toColorInt())
+val _brand_muted = Color("#d6e3ff".toColorInt())
+val _brand_onSubtle = Color("#e5eeff".toColorInt())
+val _brand_surface = Color("#fafbff".toColorInt())
+val _brand_subtle = Color("#f5f8ff".toColorInt())
+}
+
+
+
+object Action {
+
+val primary = 
+val neutral = 
+val success = 
+val danger = 
+val ghost = 
+val outline = 
+val inverse = 
+val reverseInverse = 
+}
+
+
+
+object Interaction {
+
+val default = 
+val neutral = 
+val danger = 
+val ghost = 
+val disabled = 
+val border = 
+val background = 
+val inverse = 
+val focus = 
+}
+
+
+
+object NonSemantic {
+
+val gray = 
+val white = 
+val red = 
+val magenta = 
+val purple = 
+val violet = 
+val blue = 
+val cyan = 
+val teal = 
+val aquamarine = 
+val green = 
+val orange = 
+}
+
+
+
+object Semantic {
+
+val brand = 
+val neutral = 
+val neutralAlpha = 
+val info = 
+val success = 
+val warning = 
+val danger = 
+}
+
+
+
+object CoolGray {
+
+val cool_gray_min_4 = Color("#1b242c".toColorInt())
+val cool_gray_min_3 = Color("#272e35".toColorInt())
+val cool_gray_min_2 = Color("#3a424a".toColorInt())
+val cool_gray_min_1 = Color("#4a545e".toColorInt())
+val cool_gray_0 = Color("#555f6d".toColorInt())
+val cool_gray_Plus_1 = Color("#7e8c9a".toColorInt())
+val cool_gray_Plus_2 = Color("#9ea8b3".toColorInt())
+val cool_gray_Plus_3 = Color("#cfd6dd".toColorInt())
+val cool_gray_Plus_4 = Color("#dee3e7".toColorInt())
+val cool_gray_Plus_5 = Color("#eaedf0".toColorInt())
+val cool_gray_Plus_6 = Color("#f0f3f5".toColorInt())
+val cool_gray_Plus_7 = Color("#f5f7f9".toColorInt())
+val cool_gray_Plus_8 = Color("#fcfcfd".toColorInt())
+}
+
+
+
+object CoolGrayAlpha {
+
+val cool_grayA_min_4 = Color("#09131aed".toColorInt())
+val cool_grayA_min_3 = Color("#0a121ae0".toColorInt())
+val cool_grayA_min_2 = Color("#1a232ddb".toColorInt())
+val cool_grayA_min_1 = Color("#1d2835cc".toColorInt())
+val cool_grayA_0 = Color("#182639bd".toColorInt())
+val cool_grayA_Plus_1 = Color("#1830498f".toColorInt())
+val cool_grayA_Plus_2 = Color("#0d253f66".toColorInt())
+val cool_grayA_Plus_3 = Color("#10315633".toColorInt())
+val cool_grayA_Plus_4 = Color("#04294321".toColorInt())
+val cool_grayA_Plus_5 = Color("#10284717".toColorInt())
+val cool_grayA_Plus_6 = Color("#022e500f".toColorInt())
+val cool_grayA_Plus_7 = Color("#00295c0a".toColorInt())
+val cool_grayA_Plus_8 = Color("#1f529e05".toColorInt())
+}
+
+
+
+object NeutralGray {
+
+val neutral_gray_min_4 = Color("#212121".toColorInt())
+val neutral_gray_min_3 = Color("#2e2e2e".toColorInt())
+val neutral_gray_min_2 = Color("#404040".toColorInt())
+val neutral_gray_min_1 = Color("#525252".toColorInt())
+val neutral_gray_0 = Color("#5e5e5e".toColorInt())
+val neutral_gray_Plus_1 = Color("#8a8a8a".toColorInt())
+val neutral_gray_Plus_2 = Color("#a6a6a6".toColorInt())
+val neutral_gray_Plus_3 = Color("#d6d6d6".toColorInt())
+val neutral_gray_Plus_4 = Color("#e3e3e3".toColorInt())
+val neutral_gray_Plus_5 = Color("#ebebeb".toColorInt())
+val neutral_gray_Plus_6 = Color("#f2f2f2".toColorInt())
+val neutral_gray_Plus_7 = Color("#f7f7f7".toColorInt())
+val neutral_gray_Plus_8 = Color("#fcfcfc".toColorInt())
+}
+
+
+
+object NeutralGrayAlpha {
+
+val neutral_grayA_min_4 = Color("#121212ed".toColorInt())
+val neutral_grayA_min_3 = Color("#0f0f0fe0".toColorInt())
+val neutral_grayA_min_2 = Color("#212121db".toColorInt())
+val neutral_grayA_min_1 = Color("#262626cc".toColorInt())
+val neutral_grayA_0 = Color("#262627bd".toColorInt())
+val neutral_grayA_Plus_1 = Color("#2b2b2b8f".toColorInt())
+val neutral_grayA_Plus_2 = Color("#21212166".toColorInt())
+val neutral_grayA_Plus_3 = Color("#2e2e2e33".toColorInt())
+val neutral_grayA_Plus_4 = Color("#1f1f1f21".toColorInt())
+val neutral_grayA_Plus_5 = Color("#1c1c1c17".toColorInt())
+val neutral_grayA_Plus_6 = Color("#2121210f".toColorInt())
+val neutral_grayA_Plus_7 = Color("#2e2e2e0a".toColorInt())
+val neutral_grayA_Plus_8 = Color("#52525205".toColorInt())
+}
+
+
+
+object WarmGray {
+
+val warm_gray_min_4 = Color("#222220".toColorInt())
+val warm_gray_min_3 = Color("#30302c".toColorInt())
+val warm_gray_min_2 = Color("#40403a".toColorInt())
+val warm_gray_min_1 = Color("#52524c".toColorInt())
+val warm_gray_0 = Color("#5f5f58".toColorInt())
+val warm_gray_Plus_1 = Color("#8b8b84".toColorInt())
+val warm_gray_Plus_2 = Color("#a8a89f".toColorInt())
+val warm_gray_Plus_3 = Color("#d9d9d3".toColorInt())
+val warm_gray_Plus_4 = Color("#e5e5e1".toColorInt())
+val warm_gray_Plus_5 = Color("#eaeae6".toColorInt())
+val warm_gray_Plus_6 = Color("#f3f3f1".toColorInt())
+val warm_gray_Plus_7 = Color("#f8f8f7".toColorInt())
+val warm_gray_Plus_8 = Color("#fdfdfc".toColorInt())
+}
+
+
+
+object WarmGrayAlpha {
+
+val warm_grayA_min_4 = Color("#131311ed".toColorInt())
+val warm_grayA_min_3 = Color("#12120de0".toColorInt())
+val warm_grayA_min_2 = Color("#22221bdb".toColorInt())
+val warm_grayA_min_1 = Color("#272720cc".toColorInt())
+val warm_grayA_0 = Color("#26261cbd".toColorInt())
+val warm_grayA_Plus_1 = Color("#2f2f238f".toColorInt())
+val warm_grayA_Plus_2 = Color("#28281166".toColorInt())
+val warm_grayA_Plus_3 = Color("#3e3e1d33".toColorInt())
+val warm_grayA_Plus_4 = Color("#33331421".toColorInt())
+val warm_grayA_Plus_5 = Color("#22220217".toColorInt())
+val warm_grayA_Plus_6 = Color("#2f2f0e0f".toColorInt())
+val warm_grayA_Plus_7 = Color("#2f2f2d0a".toColorInt())
+val warm_grayA_Plus_8 = Color("#52525205".toColorInt())
+}
+
+
+
+object WhiteAlpha {
+
+val whiteA_min_12 = Color("#ffffff05".toColorInt())
+val whiteA_min_11 = Color("#ffffff08".toColorInt())
+val whiteA_min_10 = Color("#ffffff0d".toColorInt())
+val whiteA_min_9 = Color("#ffffff14".toColorInt())
+val whiteA_min_8 = Color("#ffffff1a".toColorInt())
+val whiteA_min_7 = Color("#ffffff1f".toColorInt())
+val whiteA_min_6 = Color("#ffffff3d".toColorInt())
+val whiteA_min_5 = Color("#ffffff66".toColorInt())
+val whiteA_min_4 = Color("#ffffffa3".toColorInt())
+val whiteA_min_3 = Color("#ffffffad".toColorInt())
+val whiteA_min_2 = Color("#ffffffb8".toColorInt())
+val whiteA_min_1 = Color("#ffffffd1".toColorInt())
+val whiteA_0 = Color("#ffffff".toColorInt())
+}
+
+
+
+object Red {
+
+val _min_4 = Color("#4a0d0d".toColorInt())
+val _min_3 = Color("#30723a".toColorInt())
+val _min_2 = Color("#63c7c3".toColorInt())
+val _min_1 = Color("#a13636".toColorInt())
+val _0 = Color("#279e10".toColorInt())
+val _Plus_1 = Color("#285aa5".toColorInt())
+val _Plus_2 = Color("#1d7382".toColorInt())
+val _Plus_3 = Color("#f72626".toColorInt())
+val _Plus_4 = Color("#d15a5a".toColorInt())
+val _Plus_5 = Color("#fee7e7".toColorInt())
+val _Plus_6 = Color("#ffebeb".toColorInt())
+val _Plus_7 = Color("#fef5f5".toColorInt())
+val _Plus_8 = Color("#fffafa".toColorInt())
+}
+
+
+
+object Magenta {
+
+val _min_4 = Color("#410b2f".toColorInt())
+val _min_3 = Color("#671e4d".toColorInt())
+val _min_2 = Color("#892969".toColorInt())
+val _min_1 = Color("#953274".toColorInt())
+val _0 = Color("#91ae7a".toColorInt())
+val _Plus_1 = Color("#de5eb3".toColorInt())
+val _Plus_2 = Color("#ec89cb".toColorInt())
+val _Plus_3 = Color("#f7cae8".toColorInt())
+val _Plus_4 = Color("#fbd5ee".toColorInt())
+val _Plus_5 = Color("#fde2f4".toColorInt())
+val _Plus_6 = Color("#fdedf8".toColorInt())
+val _Plus_7 = Color("#fef6fb".toColorInt())
+val _Plus_8 = Color("#fffafd".toColorInt())
+}
+
+
+
+object Purple {
+
+val _min_4 = Color("#410b2f".toColorInt())
+val _min_3 = Color("#5b1e67".toColorInt())
+val _min_2 = Color("#792989".toColorInt())
+val _min_1 = Color("#853295".toColorInt())
+val _0 = Color("#9939ac".toColorInt())
+val _Plus_1 = Color("#4e82ee".toColorInt())
+val _Plus_2 = Color("#d98eec".toColorInt())
+val _Plus_3 = Color("#f4caf7".toColorInt())
+val _Plus_4 = Color("#f5d5fb".toColorInt())
+val _Plus_5 = Color("#f8e2fd".toColorInt())
+val _Plus_6 = Color("#faedfd".toColorInt())
+val _Plus_7 = Color("#fdf5ff".toColorInt())
+val _Plus_8 = Color("#fefaff".toColorInt())
+}
+
+
+
+object Violet {
+
+val _min_4 = Color("#201263".toColorInt())
+val _min_3 = Color("#39288a".toColorInt())
+val _min_2 = Color("#161617".toColorInt())
+val _min_1 = Color("#0e0406".toColorInt())
+val _0 = Color("#634eca".toColorInt())
+val _Plus_1 = Color("#927ef1".toColorInt())
+val _Plus_2 = Color("#aba2fb".toColorInt())
+val _Plus_3 = Color("#54917b".toColorInt())
+val _Plus_4 = Color("#e3e0ff".toColorInt())
+val _Plus_5 = Color("#eeebff".toColorInt())
+val _Plus_6 = Color("#f3f0ff".toColorInt())
+val _Plus_7 = Color("#f7f5ff".toColorInt())
+val _Plus_8 = Color("#fbfaff".toColorInt())
+}
+
+
+
+object Blue {
+
+val _min_4 = Color("#041f5d".toColorInt())
+val _min_3 = Color("#113997".toColorInt())
+val _min_2 = Color("#658b6e".toColorInt())
+val _min_1 = Color("#ff9375".toColorInt())
+val _0 = Color("#3061d5".toColorInt())
+val _Plus_1 = Color("#6691f4".toColorInt())
+val _Plus_2 = Color("#8eb0fb".toColorInt())
+val _Plus_3 = Color("#ccdcff".toColorInt())
+val _Plus_4 = Color("#d6e3ff".toColorInt())
+val _Plus_5 = Color("#e5eeff".toColorInt())
+val _Plus_6 = Color("#ebf0ff".toColorInt())
+val _Plus_7 = Color("#f5f8ff".toColorInt())
+val _Plus_8 = Color("#fafbff".toColorInt())
+}
+
+
+
+object Cyan {
+
+val _min_4 = Color("#002742".toColorInt())
+val _min_3 = Color("#0c636e".toColorInt())
+val _min_2 = Color("#165c8d".toColorInt())
+val _min_1 = Color("#60a0cf".toColorInt())
+val _0 = Color("#0870ba".toColorInt())
+val _Plus_1 = Color("#5699dc".toColorInt())
+val _Plus_2 = Color("#79bcf6".toColorInt())
+val _Plus_3 = Color("#c2e2ff".toColorInt())
+val _Plus_4 = Color("#cce7ff".toColorInt())
+val _Plus_5 = Color("#e0f1ff".toColorInt())
+val _Plus_6 = Color("#ebf5ff".toColorInt())
+val _Plus_7 = Color("#f0f9ff".toColorInt())
+val _Plus_8 = Color("#fafdff".toColorInt())
+}
+
+
+
+object Teal {
+
+val _min_4 = Color("#012a37".toColorInt())
+val _min_3 = Color("#7c4fc4".toColorInt())
+val _min_2 = Color("#d1d2d2".toColorInt())
+val _min_1 = Color("#196c85".toColorInt())
+val _0 = Color("#077597".toColorInt())
+val _Plus_1 = Color("#4f9eba".toColorInt())
+val _Plus_2 = Color("#7ac0dc".toColorInt())
+val _Plus_3 = Color("#b8e8f5".toColorInt())
+val _Plus_4 = Color("#c5ecf7".toColorInt())
+val _Plus_5 = Color("#e0f3fa".toColorInt())
+val _Plus_6 = Color("#e8f7fc".toColorInt())
+val _Plus_7 = Color("#f0fbff".toColorInt())
+val _Plus_8 = Color("#fafeff".toColorInt())
+}
+
+
+
+object Aquamarine {
+
+val _min_4 = Color("#012d26".toColorInt())
+val _min_3 = Color("#0d4a42".toColorInt())
+val _min_2 = Color("#15665b".toColorInt())
+val _min_1 = Color("#1a7061".toColorInt())
+val _0 = Color("#097b68".toColorInt())
+val _Plus_1 = Color("#3da496".toColorInt())
+val _Plus_2 = Color("#6ec9bf".toColorInt())
+val _Plus_3 = Color("#bee9e4".toColorInt())
+val _Plus_4 = Color("#c5f2ec".toColorInt())
+val _Plus_5 = Color("#daf6f3".toColorInt())
+val _Plus_6 = Color("#e2f8f5".toColorInt())
+val _Plus_7 = Color("#f0faf9".toColorInt())
+val _Plus_8 = Color("#fbfefe".toColorInt())
+}
+
+
+
+object Green {
+
+val _min_4 = Color("#0a2e0b".toColorInt())
+val _min_3 = Color("#b3b7b0".toColorInt())
+val _min_2 = Color("#246626".toColorInt())
+val _min_1 = Color("#2f6f2f".toColorInt())
+val _0 = Color("#347434".toColorInt())
+val _Plus_1 = Color("#55a555".toColorInt())
+val _Plus_2 = Color("#87c987".toColorInt())
+val _Plus_3 = Color("#c6ecc6".toColorInt())
+val _Plus_4 = Color("#cff2cf".toColorInt())
+val _Plus_5 = Color("#dff6df".toColorInt())
+val _Plus_6 = Color("#e6f9e6".toColorInt())
+val _Plus_7 = Color("#f4fbf4".toColorInt())
+val _Plus_8 = Color("#fbfefb".toColorInt())
+}
+
+
+
+object Orange {
+
+val _min_4 = Color("#492909".toColorInt())
+val _min_3 = Color("#3f2828".toColorInt())
+val _min_2 = Color("#a05c1c".toColorInt())
+val _min_1 = Color("#b4610e".toColorInt())
+val _0 = Color("#f59638".toColorInt())
+val _Plus_1 = Color("#f6a351".toColorInt())
+val _Plus_2 = Color("#feb871".toColorInt())
+val _Plus_3 = Color("#ffd4a8".toColorInt())
+val _Plus_4 = Color("#fcdec0".toColorInt())
+val _Plus_5 = Color("#ffe8d1".toColorInt())
+val _Plus_6 = Color("#fff0e0".toColorInt())
+val _Plus_7 = Color("#fff5eb".toColorInt())
+val _Plus_8 = Color("#fffcfa".toColorInt())
+}
+
+
+
+object Transparent {
+
+val _0 = Color("#ffffff00".toColorInt())
 }
 
