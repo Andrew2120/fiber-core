14f76cee554804ca0c2c923ca8edc4b34a8e8c0e
Andrew <39156362+Andrew2120@users.noreply.github.com>
2023-10-17 13:59:19 +0300

    Add files via upload

---
+++ src/Languages/Language.ts
@@ -0,0 +1,16 @@
+import { Property, PropertyValue, Struct, StructInstance } from '../Struct';
+import { TokenValueType } from '../Utility/Types';
+
+export interface Language {
+  name: string;
+  extension: string;
+  keywords: string[];
+  importStatements: string;
+
+  generateStructDeclaration(struct: Struct): string;
+  generateInstanceStructDeclaration(struct: Struct): string;
+  generatePropertyDeclaration(property: Property): string;
+  convertTokenTypeAndValue(tokenValueType: string, value: any): { type: string; value: string };
+  generateInstanceDeceleration(instance: StructInstance): string;
+  generateArrayOfInstancesDeceleration(instances: StructInstance[]): string;
+}
